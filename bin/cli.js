01a50a11a6ec6e1bf9810e92bf0c79139a9a1b5c
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2017-12-20 10:15:35 +1100

    fix(install): Ensure install command doesn't run default command (#6)
    Fixes #5

--- bin/cli.js
+++ bin/cli.js
@@ -17,7 +17,12 @@ const writeFileAsync = promisify(fs.writeFile);
 const configPath = findUp.sync(['html-sketchapp.config.js']);
 const config = configPath ? require(configPath) : {};
 
-const argv = require('yargs')
+const makeServer = (relativePath, port) => {
+  const servePath = path.resolve(process.cwd(), relativePath);
+  return serve(servePath, { port, silent: true });
+};
+
+require('yargs')
   .config(config)
   .config('config', 'Path to JavaScript config file', customConfigPath => require(customConfigPath))
   .usage('Usage: $0 [options]')
@@ -43,6 +48,65 @@ const argv = require('yargs')
       alias: 'v',
       describe: 'Set of named viewport sizes for symbols, e.g. --viewports.Desktop=1024x768 --viewports.Mobile=320x568'
     }
+  }, async argv => {
+    const port = argv.serve ? await getPort() : null;
+    const server = argv.serve ? makeServer(argv.serve, port) : null;
+
+    try {
+      const url = argv.file ? `file://${path.join(process.cwd(), argv.file)}` : argv.url;
+      const symbolsUrl = argv.serve ? urlJoin(`http://localhost:${String(port)}`, argv.url || '/') : url;
+      await waitOn({ resources: [symbolsUrl] });
+
+      const browser = await puppeteer.launch();
+
+      try {
+        const page = await browser.newPage();
+
+        await page.goto(symbolsUrl, { waitUntil: 'networkidle0' });
+
+        const bundlePath = path.resolve(__dirname, '../script/dist/generateAlmostSketch.bundle.js');
+        const bundle = await readFileAsync(bundlePath, 'utf8');
+        await page.addScriptTag({ content: bundle });
+
+        await page.evaluate('generateAlmostSketch.setupSymbols({ name: "html-sketchapp symbols" })');
+
+        await page.evaluate('generateAlmostSketch.snapshotColorStyles()');
+
+        const viewports = argv.viewports || { Desktop: '1024x768' };
+        const hasViewports = Object.keys(viewports).length > 1;
+        for (const viewportName in viewports) {
+          if (viewports.hasOwnProperty(viewportName)) {
+            const viewport = viewports[viewportName];
+            const [ width, height ] = viewport.split('x').map(x => parseInt(x, 10));
+            await page.setViewport({ width, height });
+            await page.evaluate(`generateAlmostSketch.snapshotTextStyles({ suffix: "${hasViewports ? `/${viewportName}` : ''}" })`);
+            await page.evaluate(`generateAlmostSketch.snapshotSymbols({ suffix: "${hasViewports ? `/${viewportName}` : ''}" })`);
+          }
+        }
+
+        const asketchDocumentJSON = await page.evaluate('generateAlmostSketch.getDocumentJSON()');
+        const asketchPageJSON = await page.evaluate('generateAlmostSketch.getPageJSON()');
+
+        const outputPath = path.resolve(process.cwd(), argv.outDir);
+        await mkdirp(outputPath);
+
+        const outputPagePath = path.join(outputPath, 'page.asketch.json');
+        const outputDocumentPath = path.join(outputPath, 'document.asketch.json');
+
+        await Promise.all([
+          writeFileAsync(outputPagePath, asketchPageJSON),
+          writeFileAsync(outputDocumentPath, asketchDocumentJSON)
+        ]);
+      } finally {
+        if (browser && typeof browser.close === 'function') {
+          browser.close();
+        }
+      }
+    } finally {
+      if (server && typeof server.stop === 'function') {
+        server.stop();
+      }
+    }
   })
   .command('install', 'Install the html-sketchapp Sketch plugin', {}, async () => {
     const { getInstalledPath } = require('get-installed-path');
@@ -50,72 +114,6 @@ const argv = require('yargs')
     const pluginPath = path.resolve(htmlSketchappPath, 'asketch2sketch.sketchplugin');
 
     const opn = require('opn');
-    opn(pluginPath);
+    opn(pluginPath, { wait: false });
   })
-  .argv;
-
-const makeServer = (relativePath, port) => {
-  const servePath = path.resolve(process.cwd(), relativePath);
-  return serve(servePath, { port, silent: true });
-};
-
-(async function() {
-  const port = argv.serve ? await getPort() : null;
-  const server = argv.serve ? makeServer(argv.serve, port) : null;
-
-  try {
-    const url = argv.file ? `file://${path.join(process.cwd(), argv.file)}` : argv.url;
-    const symbolsUrl = argv.serve ? urlJoin(`http://localhost:${String(port)}`, argv.url || '/') : url;
-    await waitOn({ resources: [symbolsUrl] });
-
-    const browser = await puppeteer.launch();
-
-    try {
-      const page = await browser.newPage();
-
-      await page.goto(symbolsUrl, { waitUntil: 'networkidle0' });
-
-      const bundlePath = path.resolve(__dirname, '../script/dist/generateAlmostSketch.bundle.js');
-      const bundle = await readFileAsync(bundlePath, 'utf8');
-      await page.addScriptTag({ content: bundle });
-
-      await page.evaluate('generateAlmostSketch.setupSymbols({ name: "html-sketchapp symbols" })');
-
-      await page.evaluate('generateAlmostSketch.snapshotColorStyles()');
-
-      const viewports = argv.viewports || { Desktop: '1024x768' };
-      const hasViewports = Object.keys(viewports).length > 1;
-      for (const viewportName in viewports) {
-        if (viewports.hasOwnProperty(viewportName)) {
-          const viewport = viewports[viewportName];
-          const [ width, height ] = viewport.split('x').map(x => parseInt(x, 10));
-          await page.setViewport({ width, height });
-          await page.evaluate(`generateAlmostSketch.snapshotTextStyles({ suffix: "${hasViewports ? `/${viewportName}` : ''}" })`);
-          await page.evaluate(`generateAlmostSketch.snapshotSymbols({ suffix: "${hasViewports ? `/${viewportName}` : ''}" })`);
-        }
-      }
-
-      const asketchDocumentJSON = await page.evaluate('generateAlmostSketch.getDocumentJSON()');
-      const asketchPageJSON = await page.evaluate('generateAlmostSketch.getPageJSON()');
-
-      const outputPath = path.resolve(process.cwd(), argv.outDir);
-      await mkdirp(outputPath);
-
-      const outputPagePath = path.join(outputPath, 'page.asketch.json');
-      const outputDocumentPath = path.join(outputPath, 'document.asketch.json');
-
-      await Promise.all([
-        writeFileAsync(outputPagePath, asketchPageJSON),
-        writeFileAsync(outputDocumentPath, asketchDocumentJSON)
-      ]);
-    } finally {
-      if (browser && typeof browser.close === 'function') {
-        browser.close();
-      }
-    }
-  } finally {
-    if (server && typeof server.stop === 'function') {
-      server.stop();
-    }
-  }
-}());
+  .parse();
